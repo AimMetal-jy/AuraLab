1478db9af6e2d8050da89400ac4dd78cd41b45d1
ScHnappi <3142953673@qq.com>
2025-07-01 15:30:25 +0800

    对无关代码进行删除

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/material.dart';
-// Flutter材料设计组件库
+import 'package:flutter/material.dart'; // Flutter材料设计组件库
 
 /// 设置页面主组件
 /// 
@@ -128,7 +127,6 @@ class _SettingsState extends State<Settings> {
             trailing: Icon(Icons.arrow_forward_ios, size: 16), // 右侧箭头图标，表示可以点击进入下一级页面
             onTap: () {},
           ),
-
 
           // 默认收起翻译设置项 - 控制是否默认收起翻译内容
           _SettingSection(
